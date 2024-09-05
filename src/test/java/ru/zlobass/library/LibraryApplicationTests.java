33d7d0925dcd41588f31db508b904e45c808b2ab
zlobasss <sergey4ik@outlook.com>
2024-09-05 15:15:25 +0700

    Init commit

---
+++ src/test/java/ru/zlobass/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zlobass.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
